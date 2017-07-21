1f385fb302eeb4b32855cba2980a99a7c58d7a46
srivastava-vishal <visha>
2017-07-21 15:43:26 +0530

    updated new files

--- main.js
+++ main.js
@@ -1,271 +1,373 @@
-					var songs = [{
-        'name': 'Aadat',
-        'artist': 'Ninja',
-        'album': 'Aadat',
-        'duration': '4:06',
-       'fileName': 'song1.mp3',
-	   'image': 'song1.jpg'
-    },
-	    {
-        'name': 'Flying Car',
-        'album': 'Evergreen',
-        'artist': 'Ninja',
-        'duration': '3:28',
-        'fileName': 'song3.mp3',
-		'image': 'song3.jpg'
-    },
-	    {
-        'name': 'Back Bone',
-        'artist': 'Hardy Sandhu',
-        'album': 'Back Bone',
-        'duration': '2:55',
-        'fileName': 'song2.mp3',
-		'image': 'song2.jpg'
-    },
-	    {
-        'name': 'All Black',
-        'artist': 'sukh E',
-        'album': 'All Black',
-        'duration': '3:45',
-        'fileName': 'song4.mp3',
-		'image': 'song4.jpg'
-		
-    },
-	 {
-        'name': 'Gabbroo',
-        'artist': 'Jassie Gill',
-        'album': 'Gabbroo',
-        'duration': '3:29',
-        'fileName': 'song5.mp3',
-		'image': 'song5.jpg'
-		
-	 }]
-					var currentSongNumber = 1;
-					var willLoop = 0;
-					var willShuffle = 0; // will use this soon
-
-					function setvolume(){
-						var song =document.querySelector('audio');
-						song.volume =slider.value/100;
-						
-					}
-					
-					//Toggle function is added
-			function toggleSong() {
-			var song = document.querySelector('audio');
-			if(song.paused == true) {
-			console.log('Playing');
-			$('.play-icon').removeClass('fa-play').addClass('fa-pause');
-			song.play();
-			}
-			else {
-			console.log('Pausing');
-			$('.play-icon').removeClass('fa-pause').addClass('fa-play');
-			song.pause();
-			}
-			}
-			
-			
-	
-	//Time format changes
-			function fancyTimeFormat(time)
-			{   
-			// Hours, minutes and seconds
-			var hrs = ~~(time / 3600);
-			var mins = ~~((time % 3600) / 60);
-			var secs = time % 60;
-
-			// Output like "1:01" or "4:03:59" or "123:03:59"
-			var ret = "";
-
-			if (hrs > 0) {
-				ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
-			}
-
-			ret += "" + mins + ":" + (secs < 10 ? "0" : "");
-			ret += "" + secs;
-			return ret;
-			}
-			
-			 function updateTimer(){
-		var song = document.querySelector('audio');
-		var ct = song.currentTime;
-		var td = song.duration;
-		var percentage = (ct/td)*100;
-		$('.progress-filled').css('width', percentage+ "%");
-	}
-			
-			function changeCurrentSongDetails(songObj) {
-    $('.current-song-image').attr('src','img/' + songObj.image);
-    $('.current-song-name').text(songObj.name);
-    $('.current-song-album').text(songObj.album);
-	
+//var currentSongNumber = 1;
+//var willLoop = 0;
+//var willShuffle = 0;
+//genre check
+var happy=0;
+var sad=0;
+var romantic=0;
+var party=0;
+function toggleSong() {
+  //function KEY WORD IS USED TO START THE function
+var song = document.querySelector('audio');
+if(song.paused == true) {
+console.log('Playing');
+$('.play-icon').removeClass('fa-play').addClass('fa-pause');
+song.play();
 }
-
-   
-     function timeJump() {
-    var song = document.querySelector('audio')
-    song.currentTime = song.duration - 5;
-     }
-	 
-	
-			
-				
-	//current time updation
-				function updateCurrentTime() {
-				var song = document.querySelector('audio');
-				//console.log(song.currentTime);
-				//console.log(song.duration);
-				var currentTime =Math.floor(song.currentTime);
-				currentTime = fancyTimeFormat(currentTime);
-				var duration = Math.floor(song.duration);
-				duration = fancyTimeFormat(duration);
-				$('.time-elapsed').text(currentTime);
-				$('.song-duration').text(duration);
-				}
-				
-				function addSongNameClickEvent(songObj,position) {
-					var id ="#song" + position;
-    var songName = songObj.fileName; 
-			$(id).click(function() {
-			var audio = document.querySelector('audio');
-			var currentSong = audio.src;
-			if(currentSong.search(songName) != -1)
-			{
-			toggleSong();
-			}
-			else {
-			audio.src = songName;
-			toggleSong();
-			 changeCurrentSongDetails(songObj); // Function Call
-			}
-			});
-			}
-			
-			 function randomExcluded(min, max, excluded) {
-    var n = Math.floor(Math.random() * (max-min) + min);
-    if (n >= excluded) n++;
-    return n;
+else {
+console.log('Pausing');
+$('.play-icon').removeClass('fa-pause').addClass('fa-play');
+song.pause();
 }
-	
-				
-				window.onload = function() {
-					
-			  changeCurrentSongDetails(songs[0]);
-				updateCurrentTime();
-				setInterval(function(){
-				updateCurrentTime();
-				updateTimer();
-				},1000);
-				//var songName1 = 'Tamma Song';
-				//var songName2 = 'Humma Song';
-				//var songName3 = 'Nashe Si Chadh Gayi';
-				//var songName4 = 'The Breakup Song';
-				// declaration of array
-				// var songList = ['love me like you do','Carnage feat','Let me love you ','Dont let me down']; 
-				 // var fileNames = ['song1.mp3','song2.mp3','song3.mp3','song4.mp3'];
-				 // var artistList = ['neha', 'monali', 'neha', 'neha']; 
-				 // var albumList = ['Badrinath ki Dulhania','Ok Jaanu','Befikre','Ae Dil Hai Mushkil'];
-					//var durationList = ['2:56','3:15','2:34','2:29'];
-					
-
-				 for(var i =0; i < songs.length;i++) {
-					var Obj = songs[i];
-					var name = '#song' + (i+1);
-					var song = $(name);
-					song.find('.song-name').text(Obj.name);
-					song.find('.song-artist').text(Obj.artist);
-					song.find('.song-album').text(Obj.album);
-					song.find('.song-length').text(Obj.duration);
-					addSongNameClickEvent(Obj,i+1)
-				}
-				
-					//addSongNameClickEvent(fileNames[0],1);
-					//addSongNameClickEvent(fileNames[1],2);
-					//addSongNameClickEvent(fileNames[2],3);
-					//addSongNameClickEvent(fileNames[3],4);
-					
-					//for (var i = 0; i < fileNames.length ; i++) {
-						//addSongNameClickEvent(fileNames[i],i)
-					//} 
-					$('#songs').DataTable({
-        paging: false
-    });
-					
-				}
-				
-		
-	
-    $('.welcome-screen button').on('click', function() {
-        var name = $('#name-input').val();
-        if (name.length > 2) {
-            var message = "Welcome, " + name;
-            $('.main .user-name').text(message);
-            $('.welcome-screen').addClass('hidden');
-            $('.main').removeClass('hidden');
-        } else {
-            $('#name-input').addClass('error');
-        }
-    });
-	
-	//click function added
-    $('.play-icon').on('click', function() {
-       toggleSong();
-    });
-	
-	//keypress function added
-    $('body').on('keypress',function(event) {
-    var target = event.target;
-    if (event.keyCode == 32 && target.tagName !='INPUT')
-    {
-        toggleSong();
-    }
+}
+$('.welcome-screen button').on('click', function() {
+    var name = $('#name-input').val();
+    if (name.length > 2) {
+        var message = "Welcome, " + name;
+        $('.main .user-name').text(message);
+        $('.welcome-screen').addClass('hidden');
+        $('.mood').removeClass('hidden');
+    } else {
+          }    //empty search box  nahi chlega koi name jiski lenghth 2 se zada hai dena pade gi
+          $('#name-input').addClass('error');
 });
 
-      
-        changeCurrentSongDetails(songs[0]); // Update Image
-        currentSongNumber = currentSongNumber + 1; // Change State
+
+$('.mood ').on('click', function() {
+  $('.mood').addClass('hidden');
+  $('.main').removeClass('hidden');
+});
+//Trigger Song on click play icon
+$('.play-icon').on('click', function() {
+  toggleSong();
+
+});
+//Trigger spacebar controls
+$('body').on('keypress', function(event) {
+  var target = event.target;
+            if (event.keyCode == 32 &&target.tagName !='INPUT') {
+            toggleSong();
+          }
+        });
+function fancyTimeFormat(time)
+{
+// Hours, minutes and seconds
+var hrs = ~~(time / 3600);
+var mins = ~~((time % 3600) / 60);
+var secs = time % 60;
+
+// Output like "1:01" or "4:03:59" or "123:03:59"
+var ret = "";
+
+if (hrs > 0) {
+    ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+}
+
+ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+ret += "" + secs;
+return ret;
+}
+        function updateCurrentTime() { //current time kaise update hoye ga
+var song = document.querySelector('audio');
+// console.log(song.currentTime);
+// console.log(song.duration);
+var currentTime = Math.floor(song.currentTime);
+currentTime = fancyTimeFormat(currentTime);
+var duration = Math.floor(song.duration);
+duration = fancyTimeFormat(duration);
+$('.time-elapsed').text(currentTime);
+$('.song-duration').text(duration);
+}
+
+
+
+var songs_happy = [{ // har play list  ka different object bna dye hai
+    'name': 'razamand',
+    'artist': 'diljit',
+    'album': 'Sardarji 2',
+    'duration': '2:56',
+   'fileName': 'song1.mp3',
+   'image': 'img1.jpg'
+},
+{
+    'name': 'nakhre',
+    'artist': 'jassi gill',
+    'album': 'single track',
+    'duration': '3:15',
+    'fileName': 'song 13.mp3',
+    'image': 'img11.jpg'
+},
+{
+    'name': 'salute vajde',
+    'artist': 'nimrat',
+    'album': 'single track',
+    'duration': '2:34',
+    'fileName': 'song9.mp3',
+    'image': 'img5.jpg'
+},
+{
+    'name': 'oh ho ho',
+    'artist': 'sukhbir',
+    'album': 'hindi medium',
+    'duration': '2:29',
+    'fileName': 'song4.mp3',
+    'image': 'img2.jpg'
+}]
+var songs_sad = [{
+    'name': 'aadat',
+    'artist': 'ninja',
+    'album': 'single track',
+    'duration': '2:56',
+   'fileName': 'song5.mp3',
+   'image': 'img4.jpg'
+},
+{
+    'name': 'teri yaad',
+    'artist': 'sarthi k',
+    'album': 'single track',
+    'duration': '3:15',
+    'fileName': 'song6.mp3',
+    'image': 'img10.jpg'
+},
+{
+    'name': 'peg pug',
+    'artist': 'deep karan',
+    'album': 'single track',
+    'duration': '2:34',
+    'fileName': 'song12.mp3',
+    'image': 'img9.jpg'
+},
+{
+    'name': 'oh kyu nhi jaan skke',
+    'artist': 'ninja',
+    'album': 'single track',
+    'duration': '2:29',
+    'fileName': 'song13.mp3',
+    'image': 'img4.jpg'
+}]
+var songs_romantic = [{
+    'name': 'razamand',
+    'artist': 'diljit',
+    'album': 'Sardarji 2',
+    'duration': '2:56',
+   'fileName': 'song1.mp3',
+   'image': 'img1.jpg'
+},
+{
+    'name': 'dil di queen',
+    'artist': 'ninja',
+    'album': 'crazy tabbar',
+    'duration': '3:15',
+    'fileName': 'song2.mp3',
+    'image': 'img4.jpg'
+},
+{
+    'name': 'palace',
+    'artist': 'harsimran',
+    'album': 'single track',
+    'duration': '2:34',
+    'fileName': 'song10.mp3',
+    'image': 'img8.jpg'
+},
+{
+    'name': 'khaab',
+    'artist': 'akhil',
+    'album': 'single track',
+    'duration': '2:29',
+    'fileName': 'song11.mp3',
+    'image': 'img7.jpg'
+}]
+var songs_party = [{
+    'name': 'lakk',
+    'artist': 'fazilpuira',
+    'album': 'single track',
+    'duration': '2:56',
+   'fileName': 'song8.mp3',
+   'image': 'img6.jpg'
+},
+{
+    'name': 'oh ho oh',
+    'artist': 'sukhbir',
+    'album': 'hindi medium',
+    'duration': '3:15',
+    'fileName': 'song4.mp3',
+    'image': 'img2.jpg'
+},
+{
+    'name': 'Hostel',
+    'artist': 'Sharry maan',
+    'album': 'single track',
+    'duration': '2:34',
+    'fileName': 'song3.mp3',
+    'image': 'img3.jpg'
+},
+{
+    'name': 'bhangra gidha',
+    'artist': 'nimrat',
+    'album': 'single track',
+    'duration': '2:29',
+    'fileName': 'song7.mp3',
+    'image': 'img5.jpg'
+}]
+
+function changeCurrentSongDetails(songObj) {
+$('.current-song-image').attr('src',songObj.image)
+$('.current-song-name').text(songObj.name)
+$('.current-song-album').text(songObj.album)
+}
+
+function addSongNameClickEvent(songObj,position) {
+  var songName = songObj.fileName;
+ var id = '#song' + position;
+ $(id).click(function() {
+ var audio = document.querySelector('audio');//audio ko select karne k badh variable audio mai rakh dega
+ var currentSong = audio.src;
+ if(currentSong.search(songName) != -1)
+  {
+   toggleSong();
+   changeCurrentSongDetails(songObj);
+  }
+ else {
+   audio.src = 'songName';
+   toggleSong();
+   changeCurrentSongDetails(songObj);
+   $('#now-playing').removeClass('main');
+   setTimeout(function(){
+     $('#now-playing').addClass('main');
+   },);
+   console.log("");
+
+    }
+  });
+}
+function whichPlaylist(){ //konsi playlist play honi hai click pae
+
+    if(happy == 1){
+
+        songs = songs_happy;
+    }
+    else if(sad == 1){
+        console.log("chill run");
+        songs = songs_sad;
+    }
+
+    else if(romantic == 1){
+        console.log("rock run");
+        songs = songs_romantic ;
+    }
+
+    else if( party== 1){
+
+        songs = songs_party ;
+    }
+
+
+
+    return songs;
+}
+function updateSongList(){
+    songs=whichPlaylist();
+    console.log(songs[1]);
+
+    for(var i =0; i < songs.length;i++) {// initally 0 se start ho kar  tab tak chle gi jab tak uski length songs.lenght s e kam hai
+       var obj = songs[i];
+       var name = '#song' + (i+1);
+       var song = $(name);
+
+
+       song.find('.song-name').text(obj.name);
+       song.find('.song-artist').text(obj.artist);
+       song.find('.song-album').text(obj.album);
+       song.find('.song-length').text(obj.duration);
+       addSongNameClickEvent(obj,i+1)
+     }
+
+     $('#songs').DataTable(
+       {
+           paging: false
+       }
+     );
+
+
+}
+
+
+//Code for genre based categorisation begins!! Using global variables and setting them here!
+
+
+$('#happy').on('click',function(){
+   if(typeof songs_table != 'undefined' ){
+      songs_table.destroy();
+   }
+    happy=1;
+    sad=romantic=party=0;
+    updateSongList();
+    $('.mood-sorting').addClass('hidden');
+    $('.content').removeClass('hidden');
+
+});
+
+
+$('#sad').on('click',function(){
+       if(typeof songs_table != 'undefined'){
+        songs_table.destroy();
+    }
+    sad=1;
+    happy=romantic=party=0;
+    updateSongList();
+    $('.mood').addClass('hidden');
+    $('.content').removeClass('hidden');
+
+});
+
+$('#romantic').on('click',function(){
+     if(typeof songs_table != 'undefined'){
+       songs_table.destroy();
+    }
+    romantic=1;
+    happy=sad=party=0;
+    updateSongList();
+    $('.mood').addClass('hidden');
+    $('.content').removeClass('hidden');
+
+});
+
+
+$('#party').on('click',function(){
+       if(typeof songs_table != 'undefined'){
+        songs_table.destroy();
+    }
+    party=1;
+    happy=sad=romantic=0;
+    updateSongList();
+    $('.mood').addClass('hidden');
+    $('.content').removeClass('hidden');
+
+});
+$('#back').on('click',function(){
+
+    $('.content').addClass('hidden');
+    $('.mood').removeClass('hidden');
     
-		  $('.fa-repeat').on('click',function() {
-			$('.fa-repeat').toggleClass('disabled')
-			willLoop = 1 - willLoop;
-		});
+$('.main').addClass('hidden');
 
-		$('.fa-random').on('click',function() {
-			$('.fa-random').toggleClass('disabled')
-			willShuffle = 1 - willShuffle;
-		});
-		
-		$('#slider').on('mousemove',function(){
-			setvolume();
-		});
+    song = document.querySelector('audio');
+          song.pause();
 
-$('audio').on('ended',function() {
-    var audio = document.querySelector('audio');
-    if (willShuffle == 1) {
-        var nextSongNumber = randomExcluded(1,4,currentSongNumber); // Calling our function from Stackoverflow
-        var nextSongObj = songs[nextSongNumber-1];
-        audio.src = nextSongObj.fileName;
-        toggleSong();
-        changeCurrentSongDetails(nextSongObj);
-        currentSongNumber = nextSongNumber;
-    }
-    else if(currentSongNumber < 4) {
-        var nextSongObj = songs[currentSongNumber];
-        audio.src = nextSongObj.fileName;
-        toggleSong();
-        changeCurrentSongDetails(nextSongObj);
-        currentSongNumber = currentSongNumber + 1;
-    }
-    else if(willLoop == 1) {
-        var nextSongObj = songs[0];
-        audio.src = nextSongObj.fileName;
-        toggleSong();
-        changeCurrentSongDetails(nextSongObj);
-        currentSongNumber =  1;
-    }
-    else {
-        $('.play-icon').removeClass('fa-pause').addClass('fa-play');
-        audio.currentTime = 0;
-    }
-})
+  })
+
+
+
+
+window.onload = function() {
+
+setInterval(function(){
+
+updateCurrentTime();
+
+},1000);
+
+
+
+
+}
